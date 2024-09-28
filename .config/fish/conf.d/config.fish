7866cd40e6b5b6b8d8cb011837e6c47e45d7bdcd
larpios <33772093+larpios@users.noreply.github.com>
2024-09-28 21:37:06 +0900

    fix(fish): config.fish is not read outside conf.d

---
+++ .config/fish/conf.d/config.fish
@@ -0,0 +1,18 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    alias ls="ls -a --color"
+    alias ll="ls -al --color"
+    alias vim="nvim"
+    alias v="nvim"
+
+    if type -q zoxide
+        zoxide init fish | source
+    end
+
+    if type -q fastfetch
+        fastfetch
+    end
+
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+end
+
