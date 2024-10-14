6f4334475cb1dee973c98200ba0c5901a770a9cd
larpios <33772093+larpios@users.noreply.github.com>
2024-10-14 09:29:09 +0900

    Fix zinit overriding zoxide's zi alias

--- modules/zoxide.sh
+++ modules/zoxide.sh
@@ -2,7 +2,10 @@
 
 if  command -v zoxide > /dev/null; then
     if [[ $SHELL == *bash* || $SHELL == *zsh* ]]; then
-        eval "$(zoxide init zsh)"
+        # Zinit uses this alias
+        unalias zi
+        cur_shell=$(basename $SHELL)
+        eval "$(zoxide init $cur_shell)"
     elif [[ $SHELL == *fish* ]]; then
         zoxide init fish | source
     fi
