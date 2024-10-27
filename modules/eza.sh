447203d6d67944bbdbef1068fddcd7b7e59a7ed1
larpios <33772093+larpios@users.noreply.github.com>
2024-10-27 15:19:33 +0900

    alias ls, ll when eza doesn't exist

--- modules/eza.sh
+++ modules/eza.sh
@@ -3,4 +3,7 @@
 if command -v eza > /dev/null; then
     alias ls='eza -a --color=auto --icons=auto'
     alias ll='eza -lah --color=auto --icons=auto'
+else
+    alias ls='ls -ah --color=auto'
+    alias ll='ls -lah --color=auto'
 fi
