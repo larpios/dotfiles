f72da705250b8a008a63a1d957d45b741e11ce4a
larpios <33772093+larpios@users.noreply.github.com>
2024-11-03 20:07:22 +0900

    update(brew): add a path to llvm in the PATH

--- modules/brew.sh
+++ modules/brew.sh
@@ -3,3 +3,9 @@
 if [[ -f /opt/homebrew/bin/brew ]]; then
     eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
+
+if [[ -d /opt/homebrew/opt/llvm/bin ]]; then
+    export PATH="/opt/homebrew/opt/llvm/bin:$PATH"
+    # export LDFLAGS="-L/opt/homebrew/opt/llvm/lib"
+    # export CPPFLAGS="-I/opt/homebrew/opt/llvm/include"
+fi
