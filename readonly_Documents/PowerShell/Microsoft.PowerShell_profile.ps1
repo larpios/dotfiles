fdf345fd3421057fc7c5186678ba871a1cce32b0
larpios <33772093+larpios@users.noreply.github.com>
2025-07-15 10:07:22 +0900

    feat: add Powershell profile

---
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,19 @@
+oh-my-posh init pwsh --config "C:\Program Files (x86)\oh-my-posh\themes\kushal.omp.json" | Invoke-Expression
+
+if (Test-path "C:\Program Files\Neovim\bin\nvim.exe")
+{
+    $nvimPath = "C:\Program Files\Neovim\bin\nvim.exe"
+} else
+{
+    $nvimPath = "C:\tools\neovim\nvim-win64\bin\nvim.exe"
+}
+
+function Open-NvimCurrentDir {
+    & $nvimPath .
+}
+
+Set-Alias -Name v -Value $nvimPath
+Set-Alias -Name v. -Value Open-NvimCurrentDir
+Set-Alias -Name g -Value git
+
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
