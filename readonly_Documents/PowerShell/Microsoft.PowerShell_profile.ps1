89b6c2e569d0d662b6db6e1af60e1411d2171ecb
larpios <33772093+larpios@users.noreply.github.com>
2026-02-06 11:36:14 +0900

    Update Documents/PowerShell/Microsoft.PowerShell_profile.ps1

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,41 +1,36 @@
 
-
-
-if (Test-path "C:\Program Files\Neovim\bin\nvim.exe")
-{
-    $nvimPath = "C:\Program Files\Neovim\bin\nvim.exe"
-} else
-{
-    $nvimPath = "C:\tools\neovim\nvim-win64\bin\nvim.exe"
-}
-
-function Open-NvimCurrentDir {
-    & $nvimPath .
-}
-
-Set-Alias -Name v -Value $nvimPath
-Set-Alias -Name v. -Value Open-NvimCurrentDir
-Set-Alias -Name g -Value git
-
-
-if (Get-Command zoxide -ErrorAction SilentlyContinue) {
-    Invoke-Expression (& { (zoxide init powershell | Out-String) })
-}
-
-
-if (Test-Path ~/work.ps1)
-{
-    . ~/work.ps1
-}
-
-
 # Initialize mise
 if (Get-Command mise.exe -ErrorAction SilentlyContinue) {
     mise activate pwsh | Out-String | Invoke-Expression
 }
 
-
-# Initialize starship
 if (Get-Command starship -ErrorAction SilentlyContinue) {
     Invoke-Expression (&starship init powershell)
+}
+
+if (Test-Path "C:\Program Files\Neovim\bin\nvim.exe") {
+    $nvimPath = "C:\Program Files\Neovim\bin\nvim.exe"
+} elseif (Test-Path "C:\tools\neovim\nvim-win64\bin\nvim.exe") {
+    $nvimPath = "C:\tools\neovim\nvim-win64\bin\nvim.exe"
+}
+
+if ($nvimPath) {
+    function Open-NvimCurrentDir {
+        & $nvimPath .
+    }
+    Set-Alias -Name v -Value $nvimPath
+    Set-Alias -Name v. -Value Open-NvimCurrentDir
+}
+
+if (Get-Command git -ErrorAction SilentlyContinue) {
+    Set-Alias -Name g -Value git
+}
+
+if (Get-Command zoxide -ErrorAction SilentlyContinue) {
+    zoxide init powershell | Invoke-Expression
+}
+
+if (Test-Path ~/work.ps1)
+{
+    . ~/work.ps1
 }
