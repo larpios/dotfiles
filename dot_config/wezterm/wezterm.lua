e1938b0b2d02b7fa2f9d2b7ba1d48846818f632c
larpios <33772093+larpios@users.noreply.github.com>
2026-03-14 04:27:09 +0900

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,23 +1,56 @@
--- Pull in the wezterm API
---
---TODO: Separate it into another file. This fails when only symlinking wezterm.lua without other files.
---      The Idea you can use is get the script path and use that for absolute paths.
-function get_os_name()
+---@diagnostic disable: undefined-global
+local function get_os_name()
 	-- ask LuaJIT first
 	if jit then
 		return jit.os
 	end
 
 	-- Unix, Linux variants
-	local fh, err = assert(io.popen("uname -o 2>/dev/null", "r"))
-	if fh then
-		osname = fh:read()
-	end
+	local fh, _err = assert(io.popen("uname -o 2>/dev/null", "r"))
 
-	return osname or "Windows"
+	local osname = fh:read() or "Windows"
+
+	return osname
 end
 
 local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+local current_os = get_os_name()
+
+local preload = {
+	--- @class QuickSelectPattern
+	--- @field name string Name of the pattern
+	--- @field desc string? Description for the pattern
+	--- @field pattern string Regex pattern string
+	--- @field open fun(text, window?, pane?) Operation to perform
+
+	--- @type QuickSelectPattern[]
+	quickselect_patterns = {
+		{
+			name = "http",
+			pattern = "^https://\\S+?",
+			open = function(text)
+				wezterm.open_with(text)
+			end,
+		},
+		{
+			name = "github_auth_code",
+			pattern = "^[A-Z0-9]{4}-[A-Z0-9]{4}$",
+			open = function(text, window)
+				window:copy_to_clipboard(text)
+			end,
+		},
+		{
+			name = "github_url",
+			pattern = "[\\w\\d\\-_]+/[\\w\\d\\-_\\.]+",
+			open = function(text)
+				local url = "https://" .. text
+				wezterm.open_with(url)
+			end,
+		},
+	},
+}
 
 wezterm.on("toggle-opacity", function(window, pane)
 	local overrides = window:get_config_overrides() or {}
@@ -30,24 +63,9 @@ wezterm.on("toggle-opacity", function(window, pane)
 	window:set_config_overrides(overrides)
 end)
 
--- This table will hold the configuration.
-local config = {}
-
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-	config = wezterm.config_builder()
-end
-
--- This is where you actually apply your config choices
--- For example, changing the color scheme:
-
--- local current_os = package.config:sub(1, 1) == "\\" and "win" or "unix"
-
-local current_os = get_os_name()
-
 -- Default shell
-local default_shell = current_os ~= "Windows" and os.getenv("SHELL") or "pwsh.exe"
+local is_windows = current_os == "Windows"
+local default_shell = is_windows or "pwsh.exe" and os.getenv("SHELL")
 config.default_prog = { default_shell }
 
 -- Colorscheme
@@ -64,15 +82,15 @@ config.font = wezterm.font_with_fallback({
 	"Courier New",
 })
 if current_os == "OSX" or current_os == "Darwin" then
-	config.font_size = 18
+	config.font_size = 16
 else
 	config.font_size = 13
 end
 -- Ligature
-config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+config.harfbuzz_features = { "calt=1", "clig=1", "liga=1" }
 
 -- Front End
-config.front_end = 'WebGpu'
+config.front_end = "WebGpu"
 
 -- FPS
 config.max_fps = tonumber(os.getenv("WEZTERM_FPS")) or 60
@@ -108,20 +126,13 @@ config.mouse_bindings = {
 
 -- Keybindings
 
--- Set a key binding to toggle the tab bar
--- @param key string: The key to bind
--- @param mods string: The modifiers to bind
--- @param action string: The action to bind
-local function set_key_binding(key, mods, action)
-	config.keys[#config.keys + 1] = { key = key, mods = mods, action = action }
-end
-
--- config.disable_default_key_bindings = true
+config.disable_default_key_bindings = false
 local act = wezterm.action
 --
 --  [ CTRL = CMD ]
 --   [ ALT = OPT ]
 config.keys = {
+	{ key = "Space", mods = "CTRL|SHIFT", action = wezterm.action.DisableDefaultAssignment },
 	{ key = "p", mods = "CTRL|ALT", action = act.ActivateCommandPalette },
 	{ key = "r", mods = "CTRL|SHIFT", action = "ReloadConfiguration" },
 	-- { key = "t", mods = "CTRL", action = act.SpawnTab("CurrentPaneDomain") },
@@ -139,20 +150,34 @@ config.keys = {
 	{ key = "B", mods = "CTRL", action = wezterm.action.EmitEvent("toggle-opacity") },
 	{
 		-- Select URL to open
-		key = 'o', mods = 'CTRL|SHIFT', action = act.QuickSelectArgs {
+		key = "o",
+		mods = "CTRL|SHIFT",
+		action = act.QuickSelectArgs({
 			label = "open",
-			patterns = { 'https?://\\S+'},
+			patterns = {
+				"\\b[\\w\\-_\\./]+\\b",
+			},
 			action = wezterm.action_callback(function(window, pane)
-				local url = window:get_selection_text_for_pane(pane)
-				wezterm.open_with(url)
-			end)
-		}
-	}
+				local text = window:get_selection_text_for_pane(pane)
+
+				for _, value in ipairs(preload.quickselect_patterns) do
+					wezterm.log_info('Try matching text "' .. text .. '" to the pattern "' .. value.name .. '".')
+					if text:match(value.pattern) then
+						value.open(text, window, pane)
+						return
+					end
+				end
+
+				-- fallback
+				window:copy_to_clipboard(text, "ClipboardAndPrimarySelection")
+			end),
+		}),
+	},
 }
 
-local dirs = { h = "Left", j = "Down", k = "Up", l = "Right" }
+local directions = { h = "Left", j = "Down", k = "Up", l = "Right" }
 
-for key, direction in pairs(dirs) do
+for key, direction in pairs(directions) do
 	-- -- Adjust pane size
 	-- config.keys[#config.keys + 1] = { key = key, mods = "ALT", action = act.AdjustPaneSize({ direction, 5 }) }
 	--
@@ -169,12 +194,34 @@ for key, direction in pairs(dirs) do
 	}
 end
 
--- # Neovim-related
+-- # Plugins
 
--- local smart_splits = require("modules.smart-splits")
--- for _, key in ipairs(smart_splits.get_keys(wezterm)) do
--- 	config.keys[#config.keys + 1] = key
--- end
+local modal = wezterm.plugin.require("https://github.com/MLFlexer/modal.wezterm")
+modal.apply_to_config(config)
+modal.set_default_keys(config)
 
--- and finally, return the configuration to wezterm
+local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
+-- you can put the rest of your Wezterm config here
+smart_splits.apply_to_config(config, {
+	-- the default config is here, if you'd like to use the default keys,
+	-- you can omit this configuration table parameter and just use
+	-- smart_splits.apply_to_config(config)
+
+	-- directional keys to use in order of: left, down, up, right
+	direction_keys = { "h", "j", "k", "l" },
+	-- if you want to use separate direction keys for move vs. resize, you
+	-- can also do this:
+	direction_keys = {
+		move = { "h", "j", "k", "l" },
+		resize = { "LeftArrow", "DownArrow", "UpArrow", "RightArrow" },
+	},
+	-- modifier keys to combine with direction_keys
+	modifiers = {
+		move = "CTRL", -- modifier to use for pane movement, e.g. CTRL+h to move left
+		resize = "META", -- modifier to use for pane resize, e.g. META+h to resize to the left
+	},
+	-- log level to use: info, warn, error
+	log_level = "info",
+})
+
 return config
