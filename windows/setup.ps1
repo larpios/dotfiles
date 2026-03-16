1983742af0b32f9391160fbe74af89ed935d2f54
larpios <33772093+larpios@users.noreply.github.com>
2026-03-16 23:23:53 +0900

    Remove binaries/x86_64-linux/eza Remove binaries/x86_64-linux/fastfetch Remove binaries/x86_64-linux/rg Remove binaries/x86_64-linux/zoxide Update windows/setup.ps1

--- windows/setup.ps1
+++ windows/setup.ps1
@@ -1,85 +1,48 @@
-$scriptDirectory = $PSScriptRoot
-$dotfilesDirectory = Split-Path -Parent $scriptDirectory
+$wingetPrograms = @(
+    'Ablaze.Floorp', # Browser
+    'wez.wezterm.nightly', # Wezterm Nightly, nightly programs probably needs the `--ignore-security-hash` flag
+    'Git.Git',
+    'Neovim.Neovim.Nightly', # Neovim
+    'Helix.Helix',
+    'twpayne.chezmoi', # Dotfiles manager
+    'Microsoft.PowerShell', # PowerShell 7
+    'Starship.Starship',
+    'ajeetdsouza.zoxide'
+    'sharkdp.bat',
+    'sharkdp.fd',
+    'sxyazi.yazi',
+    'jj-vcs.jj', # Jujutsu version control system
+    'jdx.mise', # Mise package manager for devs
+    'BurntSushi.ripgrep.GNU',
+    'DEVCOM.JetBrainsMonoNerdFont', # JetBrains Mono Nerd Font
+    'Neovide.Neovide', # VLC Media Player
+    'XnSoft.XnViewMP', # Image viewer
+    'VideoLAN.VLC', # VLC Media Player
+    'Discord.Discord', # Discord
+    'Valve.Steam',
+    'Google.Chrome.EXE', # Chrome just in case
+    'Kakao.KakaoTalk' # Kakaotalk just in case
+)
 
-$symlinkFiles = ".config"
-
-$ProgramgsToGet = "brave", "obsidian", "powershell-core", "neovim", "flow-launcher", "wezterm", "oh-my-posh", "mingw", "nerd-fonts-agave", "nerd-fonts-CascadiaCode", "ripgrep", "lazygit", "python", "zoxide"
-
-function Create-Symlink {
+function Set-UserEnv {
     param(
-        [Parameter(Mandatory)]
-        [string]$src,
-        [Parameter(Mandatory)]
-        [string]$dest,
-
-        [Alias('T')]
-        [string]$Type = "SymbolicLink",
-        [Alias('F')]
-        [switch]$Force
+        [string]$Key,
+        [string]$Value
     )
-    $params = @{
-        Type = $Type
-        Value = "$dotfilesDirectory/$src"
-        Path = "$dest"
-    }
-
-    if ($Force) {
-        $params.Add("Force", $true)
-    }
-
-    try {
-        New-Item @params
-    } catch {
-        if (-not $Force) {
-            Write-Error "Faile to create symbolic link. Use -Force to overwrite existing items."
-        } else {
-            throw
-        }
-    }
+    [System.Environment]::SetEnvironmentVariable($key, $value, "User")
 }
 
-function Install-Chocolatey
-{
-    if (Get-Command "choco" -ErrorAction SilentlyContinue) {
-        Write-Host "Chocolatey is already installed."
-        return
-    }
-    Write-Output "Installing Chocolatey..."
-    Set-ExecutionPolicy Bypass -Scope Process -Force;
-    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
-    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-    Write-Output "Successfully Installed Chocolatey!"
-}
+Write-Output 'Setting ExecutionPolicy to `Bypass`'
+# Allow all PowerShell scripts
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser
 
-function Install-Programs
-{
-    # Install Programs using Chocolatey
-    Install-Chocolatey
-    Write-Output "Installing programs using Chocolatey..."
-    choco install -y @ProgramgsToGet
-    Write-Output "Successfully Installed programs using Chocolatey!"
-}
+Write-Output 'Setting XDG_CONFIG_HOME...'
+Set-UserEnv -Key "XDG_CONFIG_HOME" -Value "$HOME\.config"
 
-function Setup-Env
-{
-    [System.Environment]::SetEnvironmentVariable("XDG_CONFIG_HOME", "$HOME\.config", "User")
-}
-
-# Make PowerShell Profile
-# New-Item $PROFILE -Force
-# Invoke-WebRequest $ProfileItem.Path -OutFile $HOME/Desktop/$ProfileItem.Name | Copy-Item -Destination $ProfileItem.Dest -Force
-
-Setup-Env
-
-Create-Symlink -src ".config" -dest "$HOME/.config"
-Create-Symlink -src "windows/Files/.wslconfig" -dest "$HOME/.wslconfig" -Type HardLink -Force
-Create-Symlink -src "windows/Files/Microsoft.PowerShell_profile.ps1" -dest $PROFILE -Force
-Create-Symlink -src "notes" -dest "$HOME/notes" -Force
-Create-Symlink -src ".gitconfig" -dest "$HOME/.gitconfig" -Force
-
-Install-Programs
-
-
-Write-Output "Done!"
-
-
+Write-Output 'Installing programs...'
+winget install @wingetPrograms --ignore-security-hash
+# foreach ($package in $wingetPrograms) {
+#     winget install 
+    
+# }
+Write-Output 'Done!'
