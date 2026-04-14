b4d9711900b4dfb2f3e369a382fb3beb8705b301
larpios <33772093+larpios@users.noreply.github.com>
2026-04-15 01:18:46 +0900

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -134,8 +134,43 @@ else
 	config.font_size = 11
 end
 
+-- Use the default hyperlink rules
+config.hyperlink_rules = wezterm.default_hyperlink_rules()
+
 -- Enable Kitty image protocol
 config.enable_kitty_graphics = true
+
+-- SSH
+config.ssh_domains = {
+  {
+    -- The name of this specific domain.  Must be unique amongst
+    -- all types of domain in the configuration file.
+    name = 'my.server',
+
+    -- identifies the host:port pair of the remote server
+    -- Can be a DNS name or an IP address with an optional
+    -- ":port" on the end.
+    remote_address = '192.168.1.1',
+
+    -- Whether agent auth should be disabled.
+    -- Set to true to disable it.
+    -- no_agent_auth = false,
+
+    -- The username to use for authenticating with the remote host
+    username = 'larpi',
+
+    -- If true, connect to this domain automatically at startup
+    -- connect_automatically = true,
+
+    -- Specify an alternative read timeout
+    -- timeout = 60,
+
+    -- The path to the wezterm binary on the remote host.
+    -- Primarily useful if it isn't installed in the $PATH
+    -- that is configure for ssh.
+    -- remote_wezterm_path = "/home/yourusername/bin/wezterm"
+  },
+}
 
 -- Create unix domain socket
 config.unix_domains = {
